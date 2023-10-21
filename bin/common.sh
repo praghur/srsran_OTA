e7a48c9a476126216599066393de5f8ffd7aece1
Dustin Maas <dmaas@flux.utah.edu>
2023-10-20 18:25:33 -0600

    Fix.

--- bin/common.sh
+++ bin/common.sh
@@ -1,6 +1,4 @@
 
 SRCDIR=/var/tmp
 CFGDIR=/local/repository/etc
-OAI_RAN_MIRROR="https://gitlab.flux.utah.edu/powder-mirror/openairinterface5g"
-OAI_CN5G_REPO="https://gitlab.eurecom.fr/oai/cn5g/oai-cn5g-fed"
-SRS_REPO="https://github.com/srsran/srsRAN"
+SRS_PROJECT_REPO="https://github.com/srsRAN/srsRAN_Project"
