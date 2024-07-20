bc6a0d2059ce0c55ebeb33dc9a4ad28223f80252
praghur <166060957+praghur@users.noreply.github.com>
2024-07-20 16:43:55 -0400

    Create restart_core.sh

---
+++ bin/restart_core.sh
@@ -0,0 +1,18 @@
+set -ex
+chmod +x restart_core
+
+sudo systemctl restart open5gs-mmed
+sudo systemctl restart open5gs-sgwcd
+sudo systemctl restart open5gs-smfd
+sudo systemctl restart open5gs-amfd
+sudo systemctl restart open5gs-sgwud
+sudo systemctl restart open5gs-upfd
+sudo systemctl restart open5gs-hssd
+sudo systemctl restart open5gs-pcrfd
+sudo systemctl restart open5gs-nrfd
+sudo systemctl restart open5gs-ausfd
+sudo systemctl restart open5gs-udmd
+sudo systemctl restart open5gs-pcfd
+sudo systemctl restart open5gs-nssfd
+sudo systemctl restart open5gs-bsfd
+sudo systemctl restart open5gs-udrd
