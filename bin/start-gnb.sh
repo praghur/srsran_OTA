54fa90835ff8b93993338a960eac54a3297a6afe
praghur <166060957+praghur@users.noreply.github.com>
2024-07-27 16:23:56 -0400

    Update start-gnb.sh

--- bin/start-gnb.sh
+++ bin/start-gnb.sh
@@ -1,6 +1,6 @@
 set -ux
 tmux new-session -d -s gnb1
-tmux send-keys 'sudo /var/tmp/srsRAN_Project/build/apps/gnb/gnb  -c /local/repository/etc/srsran/gnb_x310.yml -c /var/tmp/srsRAN_Project/configs/qam256.yml' C-m
 tmux split-window -v
 tmux select-layout even-vertical
 tmux attach-session -d -t gnb1
+sudo /var/tmp/srsRAN_Project/build/apps/gnb/gnb  -c /local/repository/etc/srsran/gnb_x310.yml
