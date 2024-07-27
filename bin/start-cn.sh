107038f647f196de8008fe074d969e90255b1986
praghur <166060957+praghur@users.noreply.github.com>
2024-07-27 16:22:31 -0400

    Update start-cn.sh

--- bin/start-cn.sh
+++ bin/start-cn.sh
@@ -2,4 +2,5 @@ set -ux
 tmux new-session -d -s cn5g
 tmux split-window -v
 tmux select-layout even-vertical
+tmux attach-session -d -t cn5g
 sudo journalctl -u open5gs-amfd -u open5gs-smfd -f --output cat
