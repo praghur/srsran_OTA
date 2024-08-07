61b3a7849963f81d8f5d16396c234c240d1b144d
praghur <166060957+praghur@users.noreply.github.com>
2024-08-07 07:52:49 -0400

    Update and rename start-gnb.sh to start-gnb1.sh

--- bin/start-gnb1.sh
+++ bin/start-gnb1.sh
@@ -3,4 +3,4 @@ tmux new-session -d -s gnb1
 tmux split-window -v
 tmux select-layout even-vertical
 tmux attach-session -d -t gnb1
-sudo /var/tmp/srsRAN_Project/build/apps/gnb/gnb  -c /local/repository/etc/srsran/gnb_x310.yml
+sudo /var/tmp/srsRAN_Project/build/apps/gnb/gnb  -c /local/repository/etc/srsran/gnb_x310_1.yml
