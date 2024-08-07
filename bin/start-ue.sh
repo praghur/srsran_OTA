968915a9ebddf56699b895420d939c34eacc4812
praghur <166060957+praghur@users.noreply.github.com>
2024-08-07 12:37:15 -0400

    Update start-ue.sh

--- bin/start-ue.sh
+++ bin/start-ue.sh
@@ -3,3 +3,25 @@ tmux new-session -d -s ue1
 tmux split-window -v
 tmux select-layout even-vertical
 tmux attach-session -d -t ue1
+sudo quectel-CM -s internet -4
+/local/repository/bin/module-off.sh
+/local/repository/bin/module-on.sh
+
+
+set -ux
+tmux new-session -d -s ue2
+tmux split-window -v
+tmux select-layout even-vertical
+tmux attach-session -d -t ue2
+sudo quectel-CM -s internet -4
+/local/repository/bin/module-off.sh
+/local/repository/bin/module-on.sh
+
+set -ux
+tmux new-session -d -s ue3
+tmux split-window -v
+tmux select-layout even-vertical
+tmux attach-session -d -t ue3
+sudo quectel-CM -s internet -4
+/local/repository/bin/module-off.sh
+/local/repository/bin/module-on.sh
