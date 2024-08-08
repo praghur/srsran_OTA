05b52871f58151fb70c08b0d4daf8ccd40ccafd7
praghur <166060957+praghur@users.noreply.github.com>
2024-08-08 07:49:16 -0400

    Update profile.py

--- profile.py
+++ profile.py
@@ -163,7 +163,7 @@ UBUNTU_IMG = "urn:publicid:IDN+emulab.net+image+emulab-ops//UBUNTU22-64-STD"
 COTS_UE_IMG = "urn:publicid:IDN+emulab.net+image+PowderTeam:cots-jammy-image"
 COMP_MANAGER_ID = "urn:publicid:IDN+emulab.net+authority+cm"
 #DEFAULT_SRSRAN_HASH = "5e6f50a202c6efa671d5b231d7c911dc6c3d86ed"
-DEFAULT_SRSRAN_HASH = "4cf7513e9fe988449aadedce3278063de761a4ac"
+DEFAULT_SRSRAN_HASH = "4ac5300d4927b5199af69e6bc2e55d061fc33652"
 OPEN5GS_DEPLOY_SCRIPT = os.path.join(BIN_PATH, "deploy-open5gs.sh")
 SRSRAN_DEPLOY_SCRIPT = os.path.join(BIN_PATH, "deploy-srsran.sh")
 
