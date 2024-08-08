5230e816ceb52a1069b0b748732036cd2bc2bdde
praghur <166060957+praghur@users.noreply.github.com>
2024-08-08 10:56:23 -0400

    Update start-ue.sh

--- bin/start-ue.sh
+++ bin/start-ue.sh
@@ -25,3 +25,11 @@ tmux attach-session -d -t ue3
 sudo quectel-CM -s internet -4
 /local/repository/bin/module-off.sh
 /local/repository/bin/module-on.sh
+
+set -ux
+tmux new-session -d -s ue4
+tmux split-window -v
+tmux select-layout even-vertical
+tmux attach-session -d -t ue4
+sudo quectel-CM -s internet -4
+/local/repository/bin/module-off.sh
