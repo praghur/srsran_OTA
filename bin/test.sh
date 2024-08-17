ff80c0e6afe031e244ddd75b1c5dc0b967a69f5b
praghur <166060957+praghur@users.noreply.github.com>
2024-08-17 19:17:30 -0400

    Update test.sh

--- bin/test.sh
+++ bin/test.sh
@@ -4,3 +4,10 @@ sudo apt-get install iperf3
 
 #At the server (destination)
 iperf3 -s  #This will cause a message called "Server listening on 5201"
+
+#At the client (source)
+iperf3 -c 10.45.0.5 -u -b 1M -t 10
+#where 10.45.0.5 is the server (destination) IP address
+# -u is UDP test
+# -b setting the bandwidth to 1Mbps
+# -t setting the duration of test to 10 seconds. 
