5f182e834916177199cd799a566d5291c1cf3b7f
praghur <166060957+praghur@users.noreply.github.com>
2024-08-17 19:42:23 -0400

    Update test.sh

--- bin/test.sh
+++ bin/test.sh
@@ -11,3 +11,13 @@ iperf3 -c 10.45.0.5 -u -b 1M -t 10
 # -u is UDP test
 # -b setting the bandwidth to 1Mbps
 # -t setting the duration of test to 10 seconds. 
+
+
+#Traceroute will provide us with latency for the data transmission
+#First install traceroute in the source and destination
+sudo apt-get update
+sudo apt-get install traceroute
+
+#At the source machine (client), run the traceroute command with -U to indicate UDP packets. 
+#The size can be changed (I did not test this) using the command traceroute -U -s 1200 10.45.0.9 where 1200 bytes of UDP packets will be sent
+traceroute -U 10.45.0.9
