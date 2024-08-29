36ca60df7377eb09904499caee58035f63f5eb89
praghur <166060957+praghur@users.noreply.github.com>
2024-08-29 16:36:47 -0400

    Create start_ue2.sh

---
+++ bin/start_ue2.sh
@@ -0,0 +1,8 @@
+set -ux
+tmux new-session -d -s ue2
+tmux split-window -v
+tmux select-layout even-vertical
+tmux attach-session -d -t ue2
+sudo quectel-CM -s internet -4
+/local/repository/bin/module-off.sh
+/local/repository/bin/module-on.sh
