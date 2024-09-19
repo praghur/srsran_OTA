0d6df707ea37994ce82380352200f8d9566e2dc8
praghur <166060957+praghur@users.noreply.github.com>
2024-09-19 19:42:50 -0400

    Update deploy-open5gs.sh

--- bin/deploy-open5gs.sh
+++ bin/deploy-open5gs.sh
@@ -68,14 +68,4 @@ chmod +x open5gs-dbctl
 ./open5gs-dbctl type 999990000000001 1  # APN type IPV4
 ./open5gs-dbctl static_ip 999990000000001 10.45.2.10
 
-##For UE3 connecting with gNB3
-./open5gs-dbctl add_ue_with_slice 999990000000002 00112233445566778899aabbccddeeff 0ed47545168eafe2c39c075829a7b61f internet 3 0x000001 # IMSI,K,OPC
-./open5gs-dbctl type 999990000000002 1  # APN type IPV4
-./open5gs-dbctl static_ip 999990000000002 10.45.3.10
-
-##For UE4 connecting with gNB3
-./open5gs-dbctl add_ue_with_slice 999990000000003 00112233445566778899aabbccddeeff 0ed47545168eafe2c39c075829a7b61f internet 3 0x000001 # IMSI,K,OPC
-./open5gs-dbctl type 999990000000003 1  # APN type IPV4
-./open5gs-dbctl static_ip 999990000000003 10.45.4.10
-
 touch $SRCDIR/open5gs-setup-complete
