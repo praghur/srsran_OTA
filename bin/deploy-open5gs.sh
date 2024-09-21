13b8718191f43d1465ff4c27aa7a50310a8b3c68
praghur <166060957+praghur@users.noreply.github.com>
2024-09-21 13:28:00 -0400

    Update deploy-open5gs.sh

--- bin/deploy-open5gs.sh
+++ bin/deploy-open5gs.sh
@@ -59,7 +59,7 @@ wget https://raw.githubusercontent.com/open5gs/open5gs/main/misc/db/open5gs-dbct
 chmod +x open5gs-dbctl
   
 ##For UE1 connecting with gNB1
-./open5gs-dbctl add_ue_with_slice 999990000000000 00112233445566778899aabbccddeeff 0ed47545168eafe2c39c075829a7b61f internet 3 0x000001 # IMSI,K,OPC
+./open5gs-dbctl add_ue_with_slice 999990000000000 00112233445566778899aabbccddeeff 0ed47545168eafe2c39c075829a7b61f internet 1 0x000001 # IMSI,K,OPC
 ./open5gs-dbctl type 999990000000000 1  # APN type IPV4
 ./open5gs-dbctl static_ip 999990000000000 10.45.1.10
 
