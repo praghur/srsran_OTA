c15c05ea625438b5c6c226664eb4c67266caacbc
praghur <166060957+praghur@users.noreply.github.com>
2024-09-29 11:49:41 -0400

    Update test.sh

--- bin/test.sh
+++ bin/test.sh
@@ -23,4 +23,13 @@ sudo apt-get install traceroute
 traceroute -U -p 33435 10.45.2.10
 
 #Import pcap files from server to VM
-scp praghur@pc14-fort.emulab.net:/var/tmp/gnb_mac.pcap /home/ubuntu/
+scp praghur@pc14-fort.emulab.net:/var/tmp/gnb1_mac.pcap /home/ubuntu/
+scp praghur@pc14-fort.emulab.net:/var/tmp/gnb1_n3.pcap /home/ubuntu/
+scp praghur@pc14-fort.emulab.net:/var/tmp/gnb2_mac.pcap /home/ubuntu/
+scp praghur@pc14-fort.emulab.net:/var/tmp/gnb2_n3.pcap /home/ubuntu/
+scp praghur@pc714.emulab.net:ogstun_capture.pcap /home/ubuntu/
+scp praghur@pc714.emulab.net:enp4s0f1_capture.pcap /home/ubuntu/
+scp praghur@pc14-fort.emulab.net:enp4s0f1_gnb1.pcap /home/ubuntu/
+scp praghur@pc14-fort.emulab.net:enp4s0f1_gnb2.pcap /home/ubuntu/
+scp praghur@ota-nuc1.emulab.net:ue1_capture.pcap /home/ubuntu/
+scp praghur@ota-nuc2.emulab.net:ue2_capture.pcap /home/ubuntu/
