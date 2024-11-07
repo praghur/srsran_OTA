3970bfbacc29d419b9ce0648fb266adfc2c6cec3
praghur <166060957+praghur@users.noreply.github.com>
2024-11-06 22:20:01 -0500

    Update test.sh

--- bin/test.sh
+++ bin/test.sh
@@ -39,3 +39,7 @@ scp praghur@pc716.emulab.net:enp4s0f1_gnb1.pcap /home/ubuntu/
 scp praghur@pc714.emulab.net:enp4s0f1_gnb2.pcap /home/ubuntu/
 scp praghur@ota-nuc3.emulab.net:ue1_capture.pcap /home/ubuntu/
 scp praghur@ota-nuc4.emulab.net:ue2_capture.pcap /home/ubuntu/
+
+#To copy from local VM to remote server --> Do thi in the local VM
+scp SNRcalc.py praghur@pc06-fort.emulab.net:/tmp/
+
