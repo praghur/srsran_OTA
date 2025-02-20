14a52f677542e7bb5159f02ddc97c765068552c8
praghur <166060957+praghur@users.noreply.github.com>
2025-02-20 10:41:26 -0500

    Update and rename extractsnr.sh to extractlogs.sh

--- etc/srsran/extractlogs.sh
+++ etc/srsran/extractlogs.sh
@@ -3,9 +3,11 @@
 # Define the log file and output CSV files
 log_file="/home/ubuntu/gnb1.log"
 output_csv1="/home/ubuntu/snr_logs.csv"
+output_csv2="/home/ubuntu/harq_logs.csv"
 
 # Print the CSV headers
 echo "Log Line" > $output_csv1
+echo "Log Line" > $output_csv2
 
 # Read the log file line by line
 while IFS= read -r line; do
@@ -14,6 +16,12 @@ while IFS= read -r line; do
         # Append the entire line to the first CSV file
         echo "$line" >> $output_csv1
     fi
+
+    # Check if the line contains "h_id=" and does not contain the date. This is done to reduce traffic
+    if [[ $line == *"h_id="* && $line != *"2025-"* ]]; then
+    # Append the entire line to the second CSV file
+    echo "$line" >> $output_csv2
+    fi
 done < "$log_file"
 
-echo "Extraction complete. The CSV files are saved at $output_csv1"
+echo "Extraction complete. The CSV files are saved at $output_csv1 and $output_csv2"
